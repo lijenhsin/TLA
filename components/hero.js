73ba2dd7c5f0b16a905e2e6d9ffbf5c71ed2f99d
Frank Hsin <lijenhsin@users.noreply.github.com>
2023-03-22 10:35:23 +0800

    Changes to be committed: 	modified:   ../components/hero.js 	modified:   ../css/tailwind.css

--- components/hero.js
+++ components/hero.js
@@ -20,13 +20,11 @@ export default function Hero() {
             <h1 className={h1class}>
               最新活動: 2023 台灣喉科醫學會春季學術研討會
             </h1>
-            <div
-              className={`p-5 py-5 text-md leading-normal text-gray-500 lg:text-xl xl:text-2xl dark:text-gray-300 `}
-            >
+            <div className="p-5 text-md leading-normal text-gray-500 lg:text-xl xl:text-2xl dark:text-gray-300">
               <p className="p-5 bg-gray-100 dark:bg-gray-900">
                 台灣喉科醫學會春季演講會精彩開講！本次演講主題為喉部經口手術研討，以及特殊病例的研討。內容精彩，歡迎您的加入！。
               </p>
-              <ul className="pl-5 lg:pl-10 list-disc pt-5 dark:text-sky-200 text-slate-800 text-sm sm:text-md lg:text-lg">
+              <ul className="pl-20 lg:pl-10 list-disc pt-5 dark:text-sky-200 text-slate-800 text-sm sm:text-md lg:text-lg">
                 <li>主辦：台灣喉科醫學會。</li>
                 <li>主辦：台北三軍總醫院。</li>
                 <li> 時間：2023 年 4 月 8 日 (六)</li>
@@ -34,11 +32,11 @@ export default function Hero() {
 
                 <li> 地址：114 台北市內湖區成功路二段 325 號。</li>
               </ul>
-              <div className="m-5 text-red-500 sm:text-sm lg:text-xl">
-                {" "}
+              <div className="m-5 text-red-500 sm:text-sm lg:text-xl text-center border-b-2 border-red-400">
+              
                 已審核台灣耳鼻喉頭頸外科學分1.5分
               </div>
-              <div className="w-full lg:h-96 md:h-80 sm:h-80 h-64">
+              <div className="w-full lg:h-96 md:h-80 sm:h-80 h-64 content-center">
                 <iframe
                   src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3613.85534276075!2d121.58890171511423!3d25.072891642885107!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442ac8581a0b6e9%3A0x7d52e36317cbca1c!2zMTE05Y-w5YyX5biC5YWn5rmW5Y2A5oiQ5Yqf6Lev5LqM5q61MzI16Jmf!5e0!3m2!1szh-TW!2stw!4v1679379787093!5m2!1szh-TW!2stw"
                   width="100%"
@@ -46,12 +44,12 @@ export default function Hero() {
                   allowfullscreen=""
                   loading="lazy"
                   referrerpolicy="no-referrer-when-downgrade"
-                  className="m-3 shadow-lg"
+                  className=" border-2 border-black shadow-lg"
                 ></iframe>
               </div>
             </div>
 
-            <div className="place-content-center flex flex-col items-start space-y-3 sm:space-x-4 sm:space-y-0 sm:items-center sm:flex-row">
+            <div className="justify-center mt-5 flex space-y-3 sm:space-x-4 sm:space-y-0  ">
               <Link
                 href="currentevent"
                 className="w-40 p-2  font-medium text-center text-white bg-blue-700 rounded-md "
@@ -59,17 +57,20 @@ export default function Hero() {
                 報名
               </Link>
             </div>
-            <div>
-              <table className="mt-10">
-                <thead className="bg-blue-300 dark:bg-blue-700">
-                  <tr>
+
+          </div>
+        </div>
+        <div className="items-center justify-center w-full lg:w-1/2 lg:pl-5">
+              <table className="mt-10 w-full">
+                <thead className="bg-blue-500 dark:bg-blue-700 text-white">
+                  <tr className="p-1">
                     <th>Time</th>
                     <th>Topic</th>
                     <th>Speaker</th>
                     <th>Moderator</th>
                   </tr>
                 </thead>
-                <tbody className="bg-sky-100 dark:bg-sky-900">
+                <tbody className="bg-gradient-to-t from-sky-300 to-blue-200 dark:bg-sky-900">
                   <tr>
                     <td>08:30 – 08:40</td>
                     <td colspan="3">Registration 報到</td>
@@ -142,13 +143,27 @@ export default function Hero() {
                       <br />
                       童綜合醫院
                     </td>
-                  </tr>{" "}
+                  </tr>
+                  <tr><td>10:30 - 10:45</td><td colspan="3">Coffee Break</td></tr>
+                  <tr>
+                    <td>10:45 - 11:45</td>
+                    <td>
+                    Case Conference and Disscussion<br/>喉頭頸特殊/轉介病例討論
+                    </td>
+                    <td colspan="2">
+                     
+                    莊凱棻醫師、朱永祥醫師（三軍總醫院）<br/>李國森醫師（台北馬偕醫院）
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>11:45 --</td>
+                    <td>Closing Remark 閉幕式</td>
+                    <td colspan="2"> 方端仁理事長</td>
+                  </tr>
                 </tbody>
               </table>
             </div>
-          </div>
-        </div>
-        <div className="items-center justify-center w-full lg:w-1/2 ">
+        <div className="mt-10 items-center justify-center w-full lg:w-1/2 ">
           <div className=" border-blue-200 border-10 p-3 shadow-lg">
             <Image
               src={heroImg}
